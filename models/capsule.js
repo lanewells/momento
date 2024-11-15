f241d1a39b4799e1b91092e5dd5f00f96a84d866
lanewells <wellsdelaneyk@gmail.com>
2024-11-15 14:10:40 -0800

    Created capsule schema/model

---
+++ models/capsule.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const capsuleSchema = mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false
+  },
+  sealDate: { type: Date, required: false },
+  releaseDate: { type: Date, required: true },
+  status: {
+    type: String,
+    enum: ["sealed", "pending seal", "released"],
+    default: "pending seal",
+    required: false
+  },
+  contents: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Item", required: true }
+  ]
+})
+
+const Capsule = mongoose.model("Capsule", capsuleSchema)
+module.exports = Capsule
