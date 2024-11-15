9f1199a314873f87dba48a15f4086291aeecad35
alinelmuller <alinelmuller@gmail.com>
2024-11-15 14:57:01 -0700

    Items

---
+++ models/item.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true, 
+      trim: true, // Removes extra whitespace
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    src: {
+      type: String,
+      trim: true,
+    },
+    altText: {
+      type: String,
+      trim: true,
+    },
+    capsule_id: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+module.exports = mongoose.model("Item", ItemSchema);
